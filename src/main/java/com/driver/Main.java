dbbdbabf390d0df0c0eb0f2a7770ec0eb3f155e0
NikhilVikhe7 <nikhikvikhe7@gmail.com>
2023-02-09 15:51:32 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+//        obj.name = "hi";
+//        System.out.println(obj.name);
+//        java: name has private access in com.driver.RWOnly
+
+        obj.setName("Nik");
+        obj.getName();
+    }
 }
